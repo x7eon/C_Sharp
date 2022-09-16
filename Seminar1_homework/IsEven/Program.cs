b35d55b09e593de0efd596567af1c0e3d394f989
Alex <sungromm@gmail.com>
2022-09-16 21:06:40 +0300

    added IsEven

---
+++ Seminar1_homework/IsEven/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+
+if (double.TryParse(Console.ReadLine(), out double Number))
+{
+    if (Number == 0) 
+    {
+        Console.WriteLine("Введите число, отличное от нуля");
+    }
+    else if (Number % 2 == 0) 
+    {
+        Console.WriteLine("Число " + Number + " четное");
+    }
+    else 
+    {
+        Console.WriteLine("Число " + Number + " нечетное");
+    }
+}
+else Console.WriteLine("Ошибка ввода! Введите число!");
