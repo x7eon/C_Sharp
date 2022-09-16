195e237debcda098d9f42ec1a5747914b55de48a
Alex <sungromm@gmail.com>
2022-09-16 21:59:49 +0300

    added program EvenNumberToN

---
+++ Seminar1_homework/EvenNumbersToN/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число: ");
+dynamic evenNum = 2;
+if(double.TryParse(Console.ReadLine(), out double Number))
+{
+    if(Number > 1)
+    {   while(evenNum <= Number)
+    {
+        Console.WriteLine(evenNum);
+        evenNum = evenNum +2;
+    }
+    }
+    else
+    {
+        Console.WriteLine("Ошибка! Введите число больше 1");
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода! Введите число");
+}
