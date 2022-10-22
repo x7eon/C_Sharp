5e4e1340b23bd3aa4c4b4a1f61b9cf0814c0579f
Alex <sungromm@gmail.com>
2022-10-22 19:31:35 +0300

    fixed names

--- Seminar2/task1/Program.cs
+++ Seminar2/task1/Program.cs
@@ -1,7 +1,7 @@
-﻿int number = new Random().Next(100, 1000); // 124
+﻿int number = new Random().Next(100, 1000);
 
 Console.WriteLine(number);
 int first = number / 100;
-int second = number % 10;
+int last = number % 10;
 
-Console.WriteLine($"{first}{second}");
+Console.WriteLine($"{first}{last}");
