d5c9ef15f2fe136f1d50f2508123fa76f269539f
Alex <sungromm@gmail.com>
2022-10-22 23:31:55 +0300

    added solve

---
+++ Seminar2/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int firstNum = new Random().Next(1, 100);
+int secondNum = new Random().Next(1, 10);
+Console.WriteLine(firstNum);
+Console.WriteLine(secondNum);
+
+if (firstNum % secondNum == 0) 
+{
+    Console.WriteLine($"{secondNum} является кратным {firstNum}");
+}
+else
+{
+    Console.WriteLine(firstNum % secondNum);
+}
