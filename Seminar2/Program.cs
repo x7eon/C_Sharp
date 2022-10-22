4ffbd03e5c2d5b1e5ea3bbdb3ea2c5e2b4ef1c3b
Alex <sungromm@gmail.com>
2022-10-22 15:57:59 +0300

    Added cycle input

---
+++ Seminar2/Program.cs
@@ -0,0 +1,13 @@
+﻿double userNum;
+
+while(true) 
+{
+    Console.WriteLine("Введите число");
+    if (double.TryParse(Console.ReadLine(), out userNum))
+    {
+        break;
+    }
+    Console.WriteLine("Ошибка ввода, введите число");
+}
+
+Console.WriteLine($"A={userNum}");
