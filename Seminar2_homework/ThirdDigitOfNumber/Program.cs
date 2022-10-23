f79888849441e91f4b88578cac2381d51f07140f
Alex <sungromm@gmail.com>
2022-10-23 15:20:57 +0300

    added solve

---
+++ Seminar2_homework/ThirdDigitOfNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = new Random().Next(1, 10000);
+Console.WriteLine(number);
+
+if(number / 100 == 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+    return;
+}
+int newNumber = number;
+while(newNumber / 1000 !=0)
+{
+    newNumber = newNumber / 10;
+}
+int result = newNumber % 10;
+Console.WriteLine(result);
