c2b95b8fd3c26823a9f6b07172401cb6f8198db4
Alex <sungromm@gmail.com>
2022-10-23 17:11:49 +0300

    fixed code

--- Seminar2_homework/ThirdDigitOfNumber/Program.cs
+++ Seminar2_homework/ThirdDigitOfNumber/Program.cs
@@ -1,4 +1,4 @@
-﻿int number = new Random().Next(1, 10000);
+﻿int number = new Random().Next(1, 100000);
 Console.WriteLine(number);
 
 if(number / 100 == 0)
@@ -6,10 +6,9 @@ if(number / 100 == 0)
     Console.WriteLine("Третьей цифры нет");
     return;
 }
-int newNumber = number;
-while(newNumber / 1000 !=0)
+while(number / 1000 !=0)
 {
-    newNumber = newNumber / 10;
+    number = number / 10;
 }
-int result = newNumber % 10;
+int result = number % 10;
 Console.WriteLine(result);
