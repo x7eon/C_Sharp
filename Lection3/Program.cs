957f6002433c017fb85628397e0ce3bec5c7bca4
Alex <sungromm@gmail.com>
2022-10-25 15:42:46 +0300

    added new methods

--- Lection3/Program.cs
+++ Lection3/Program.cs
@@ -1,4 +1,5 @@
-﻿/*for (int i = 2; i <= 10; i++)
+﻿/*
+for (int i = 2; i <= 10; i++)
 {
     for (int j = 2; j <= 10; j++)
     {
@@ -8,6 +9,7 @@
 }
 */
 
+/*
 string text = "— Я же думаю, — сказал князь, улыбаясь, — что, "
             + "ежели бы вас послали вместо нашего милого Винценгероде, "
             + "вы бы взяли приступом согласие прусского короля. "
@@ -34,3 +36,53 @@ Console.WriteLine(newText);
 Console.WriteLine();
 newText = Replace(newText, 'С', 'с');
 Console.WriteLine(newText);
+*/
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+void SelectionSortMin(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i ++)  // 1 5 4 3 2 6 7 1 1 
+    {
+        int maxPosition = i;
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+
+SelectionSortMin(arr);
+
+PrintArray(arr);
