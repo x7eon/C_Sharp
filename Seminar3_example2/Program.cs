b8144e5b0b8c4b1d3ecc299a3e83005de1e69822
Alex <sungromm@gmail.com>
2022-10-26 21:40:00 +0300

    added methods

---
+++ Seminar3_example2/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+int X = 0, Y = 0;
+
+InitCoordinatsByUser(ref X, ref Y);
+PrintQuarterByCoordinats(X, Y);
+
+
+void InitCoordinatsByUser(ref int x, ref int y)
+{
+    try
+    {
+        Console.WriteLine("Введите X: ");
+        x = int.Parse(Console.ReadLine() ?? "");
+
+        Console.WriteLine("Введите Y: ");
+        y = int.Parse(Console.ReadLine() ?? "");
+        
+    }
+
+    catch(Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода! {exc.Message}");
+        return;
+    } 
+}
+
+void PrintQuarterByCoordinats(int x, int y)
+{   
+    if (x > 0 && y < 0) Console.WriteLine("Точка находится в плоскости 4");
+    else if (x > 0 && y > 0) Console.WriteLine("Точка находится в плоскости 1");
+    else if (x < 0 && y > 0)  Console.WriteLine("Точка находится в плоскости 2");
+    else if (x < 0 && y < 0) Console.WriteLine("Точка находится в плоскости 3");
+    else Console.WriteLine("Ошибка, точка находится в начале координат");
+}
+
