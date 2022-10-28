de8485078ebb1818a90141411ffc6b47dbd03f5e
Alex <sungromm@gmail.com>
2022-10-28 23:56:03 +0300

    added solve

---
+++ Seminar3_homework/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿void DistanceOfTwoPoints3D ()
+{
+    Console.WriteLine("Введите x-координату первой точки: ");
+    int x1 = int.Parse(Console.ReadLine()?? "");
+    Console.WriteLine("Введите y-координату первой точки: ");
+    int y1 = int.Parse(Console.ReadLine()?? "");
+    Console.WriteLine("Введите z-координату первой точки: ");
+    int z1 = int.Parse(Console.ReadLine()?? "");
+    Console.WriteLine("Введите x-координату второй точки: ");
+    int x2 = int.Parse(Console.ReadLine()?? "");
+    Console.WriteLine("Введите y-координату второй точки: ");
+    int y2 = int.Parse(Console.ReadLine()?? "");
+    Console.WriteLine("Введите z-координату второй точки: ");
+    int z2 = int.Parse(Console.ReadLine()?? "");
+
+    double result = Math.Sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) + (z2 - z1)*(z2 - z1));
+    Console.WriteLine($"Расстояние между точками равно {result.ToString("0.00")}");
+    
+}
+
+DistanceOfTwoPoints3D();
