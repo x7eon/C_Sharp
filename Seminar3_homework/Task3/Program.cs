9448ba1f0b20412a5d74d74c6619a01df98b0471
Alex <sungromm@gmail.com>
2022-10-30 15:03:35 +0300

    added solve

---
+++ Seminar3_homework/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿void TableOfCube ()
+{
+    Console.WriteLine("Введите число: ");
+    int number = 0;
+    int.TryParse(Console.ReadLine(), out number);
+    for (int i = 1; i <= number; i++)
+    {
+        int result = (i * i * i);
+        Console.WriteLine(result);
+    }
+}
+
+TableOfCube();
