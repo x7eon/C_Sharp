4d89f214a7aed5887d0339d9378e3bbde7e1477e
Alex <sungromm@gmail.com>
2022-11-03 00:28:53 +0300

    added solve to task printArrayOf8UserNumbers

---
+++ Seminar4_homework/Task2_ArrayOf8Elements/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] array = new int[8];
+FillArray();
+PrintArray();
+
+int GetNumberByUser(string msg, string errorMsg)
+{   
+    Console.WriteLine(msg);
+    while(true)
+    {
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect) 
+            return userNumber;
+        Console.WriteLine(errorMsg);
+    }
+}
+
+void FillArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = GetNumberByUser("Введите число " , "Ошибка ввода!");
+    }
+}
+
+
+
+void PrintArray()
+{   
+    Console.WriteLine();
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+
+
