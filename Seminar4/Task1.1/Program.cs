c113fb3beb2f947cefff012635dcc59160eb7330
Alex <sungromm@gmail.com>
2022-11-02 20:47:24 +0300

    added solve of task HowMuchDigitsOfNumber

---
+++ Seminar4/Task1.1/Program.cs
@@ -0,0 +1,37 @@
+﻿int num = GetNumFromUser("Введите число: ", "Ошибка ввода");
+int numberOfDigits = GetNumberOfDigits(num);
+Console.WriteLine($"Количество чисел в числе {num} = {numberOfDigits}");
+
+int GetNumFromUser(string massege, string errorMassage)
+{
+    while(true)
+    {
+        Console.WriteLine(massege);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNum);
+        if(isCorrect) return userNum;
+        Console.WriteLine(errorMassage);
+    }
+}
+
+int GetNumberOfDigits(int number)
+{
+    int count = 1;
+    if (number > 0)
+    {
+        while(number > 9)
+        {
+            count += 1;
+            number /= 10; 
+        }
+    return count;
+    }
+    else
+    {
+        while(number < -9)
+        {
+            count++;
+            number /= 10;
+        }
+    return count;
+    }
+}
