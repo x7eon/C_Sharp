0d7527d001fec9fc288f9cceb7d30cfd269c1f3c
Alex <sungromm@gmail.com>
2022-11-02 23:11:59 +0300

    added solve ProductFromOneToNumber

---
+++ Seminar4/Task1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿int number = GetNumberFromUser("Введите число","Ошибка ввода!");
+int productFromOneToNumber = ProductFromOneToNumber(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} равно {productFromOneToNumber}");
+
+
+int GetNumberFromUser(string msg, string errorMsg)
+{
+    while(true)
+    {
+        Console.WriteLine(msg);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect) return userNumber;
+        Console.WriteLine(errorMsg);
+    }
+}
+
+
+int ProductFromOneToNumber(int number)
+{   
+    int product = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        product *= i;
+    }
+    return product;
+}
