39920465951c3f2e53398a32d555f8884a8b5eb9
Alex <sungromm@gmail.com>
2022-11-02 20:10:18 +0300

    added solve for task sumNumbersAndSuumDigits

---
+++ Seminar4/Example1/Program.cs
@@ -0,0 +1,41 @@
+﻿int num = GetNumberFromUser("Введите число: " , "Ошибка ввода!");
+Console.WriteLine("сумма чисел от 1 до N");
+int sumNumbers = GetSumNumbers(num);
+Console.WriteLine($"\t{num} -> {sumNumbers}");
+Console.WriteLine("сумма цифр в числе равна");
+Console.WriteLine($"\t{num} -> {GetSumDigits(num)}");
+
+
+int GetNumberFromUser(string msg , string errorMsg)
+{
+    while(true)
+    {   
+        Console.Write(msg);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMsg);
+    }
+}
+
+int GetSumNumbers(int num)
+{
+    int sum = 0;
+    while(num > 0)
+    {
+        sum += num;
+        num --;
+    }
+    return sum;
+}
+
+int GetSumDigits(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
