72d277c45b718fcaecc7d21d0206355fcaae418b
Alex <sungromm@gmail.com>
2022-11-02 23:54:52 +0300

    added solve of task numberAtoThePowerNumberB

---
+++ Seminar4_homework/Task1_NumberAToThePowerB/Program.cs
@@ -0,0 +1,31 @@
+﻿int numberByUser1 = GetNumberByUser("Введите первое число: ", "Ошибка ввода");
+int numberByUser2 = GetNumberByUser("Введите второе число: ", "Ошибка ввода");
+int result = NumberToPower(numberByUser1, numberByUser2);
+Console.WriteLine($"Число {numberByUser1} в степени {numberByUser2} = {result}");
+
+
+
+int GetNumberByUser(string msg, string errorMsg)
+{   
+    while(true)
+    { 
+        Console.WriteLine(msg);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect) 
+            return userNumber;
+        Console.WriteLine(errorMsg);
+
+    }
+}
+
+int NumberToPower(int number1, int number2)
+{   
+    int count = 1;
+    int powerResult = 1;
+    while(count <= number2)
+    {   
+        powerResult *= number1;
+        count++;
+    }
+    return powerResult;
+}
