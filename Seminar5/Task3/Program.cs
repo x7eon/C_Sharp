110e394e039be751ed73866244c86e6d8c03d010
Alex <sungromm@gmail.com>
2022-11-05 22:52:05 +0300

    added solve

---
+++ Seminar5/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] arrayRandom = GetArrayRandom(6, -9, 9);
+Console.WriteLine(String.Join(" ", arrayRandom));
+int number = -5;
+bool answer = IsNumberInArray(arrayRandom, number);
+string solve = PrintAnswer(answer);
+Console.WriteLine(solve);
+
+int[] GetArrayRandom(int size, int min, int max)
+{
+    int[] collection = new int[size];
+    for(int i = 0; i < size; i ++)
+    {
+        collection[i] = new Random().Next(min, max + 1);
+    }
+    return collection;
+}
+
+bool IsNumberInArray(int[] array, int number)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] == number)
+        {
+            return true;
+        }   
+    }
+    return false;
+}
+
+string PrintAnswer(bool TrueOrFalse)
+{
+    if (TrueOrFalse == true) return $"Массив содержит число {number}";
+    else return $"Массив НЕ содержит число {number}";
+}
