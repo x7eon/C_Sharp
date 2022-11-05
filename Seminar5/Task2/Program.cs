3ddf1ae850b70f1f39120571105243774b9beac1
Alex <sungromm@gmail.com>
2022-11-05 22:28:53 +0300

    added solve

---
+++ Seminar5/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+int[] ChangeNumberSignInArray(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        col[i] *= -1;
+    }
+    return col;
+}
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] collection = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        collection[i] = new Random().Next(min, max + 1);
+    }
+    return collection;
+}
+
+int[] newArrayRandom = GetArray(5, -9, 9);
+Console.WriteLine(String.Join(" ", newArrayRandom));
+
+int[] changedArray = ChangeNumberSignInArray(newArrayRandom);
+Console.WriteLine(String.Join(" ", changedArray));
+
