47fb5b4fb1cc6873657821483f8697857f0ce51b
Alex <sungromm@gmail.com>
2022-11-10 23:41:09 +0300

    added solve

---
+++ Seminar6/Task2DecimalToBinary/Program.cs
@@ -0,0 +1,27 @@
+﻿int userNumber = GetUserNumber("Введите число", "Ошибка ввода!");
+char[] binaryOfUserNumber = GetBinary(userNumber);
+Console.WriteLine(binaryOfUserNumber);
+
+int GetUserNumber(string msg, string errorMsg)
+{
+    while(true)
+    {
+        Console.WriteLine(msg);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect) return userNumber;
+        else Console.WriteLine(errorMsg);
+    }
+}
+
+
+
+char[] GetBinary(int num) //65
+{   
+    string answer = String.Empty;
+    while(num > 0)
+    {
+    answer += num % 2;
+    num /= 2;
+    }
+    return answer.Reverse().ToArray();;
+}
