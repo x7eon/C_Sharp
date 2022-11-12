30dfbb17cbc04fc773761daa5da0e70893ee5003
Alex <sungromm@gmail.com>
2022-11-12 17:17:08 +0300

    added solve

---
+++ Seminar6/Task3FibonacciNoRec/Program.cs
@@ -0,0 +1,30 @@
+﻿int userNumber = GetUserNumber("Введите число", "Ошибка ввода!");
+Fibonacci(userNumber);
+
+int GetUserNumber(string msg, string errorMsg)
+{
+    while (true)
+    {
+        Console.WriteLine(msg);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect) return userNumber;
+        Console.WriteLine(errorMsg);
+    }
+}
+
+
+void Fibonacci(int num)
+{   
+    int num0 = 0;
+    int num1 = 1;
+    Console.Write($"{num0} ");
+    Console.Write($"{num1} ");
+    for (int i = 0; i < num; i++)
+    {
+    int num2 = num0 + num1;
+    Console.Write($"{num2} ");
+    num0 = num1;
+    num1 = num2;
+    }
+
+}
