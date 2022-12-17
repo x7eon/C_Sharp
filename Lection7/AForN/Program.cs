71ae2ca1b81f78ca30ab2d9b216417ccfdc26dc1
Alex <sungromm@gmail.com>
2022-12-17 13:58:19 +0300

    added recursion solve

--- Lection7/AForN/Program.cs
+++ Lection7/AForN/Program.cs
@@ -7,18 +7,27 @@ Console.WriteLine("Введите n:");
 int n = int.Parse(Console.ReadLine()?? "");
 int result = AForN(a, n);
 Console.WriteLine($"{a} в степени {n} = {result}");
+Console.WriteLine();
+int resultRec = AForNRec(a, n);
+Console.WriteLine($"(рекурсия) {a} в степени {n} = {resultRec}");
+
 
 //итеративный подход, решение
 
 int AForN(int a, int n)
 {
     int result = 1;
-    int i = 0;
-    while(i < n)
+    for(int i = 0; i < n; i++)
     {
         result = result * a;
-        i++;
     }
     return result;
 }
 
+// Рекурсивный подход, решение с помощью рекурсии
+
+int AForNRec(int a, int n)
+{
+    if (n == 0) return 1;
+    else return AForN(a, n - 1) * a;
+}
