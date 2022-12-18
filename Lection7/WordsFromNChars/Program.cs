1f5419df947bec2c0f80b5a8b1da3783a543432a
Alex <sungromm@gmail.com>
2022-12-19 00:11:12 +0300

    first version

---
+++ Lection7/WordsFromNChars/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+string[] s = { "с", "и", "к", "о" };
+
+int count = s.Length;
+int n = 1;
+for (int i = 0; i < count; i++)
+{
+    for (int j = 0; j < count; j++)
+    {
+        Console.WriteLine($"{n++,-5}{s[i]}{s[j]}");
+    }
+}
