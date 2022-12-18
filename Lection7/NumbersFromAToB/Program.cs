851d3812e6e28ed347c5320615cd8800cd2daa0f
Alex <sungromm@gmail.com>
2022-12-18 18:40:27 +0300

    added another solves

--- Lection7/NumbersFromAToB/Program.cs
+++ Lection7/NumbersFromAToB/Program.cs
@@ -36,10 +36,26 @@ Console.WriteLine("Введите b: ");
 int b = int.Parse(Console.ReadLine()?? "");
 
 Console.WriteLine(StringNumFromAToBRec(a, b));
+Console.WriteLine(ReverseNums(a, b));
+Console.WriteLine(Nums(a, b));
 
+//от a до b
 string StringNumFromAToBRec(int a, int b)
 {   
     if(a <= b) return $"{a} " + StringNumFromAToBRec(a + 1, b);
     else return String.Empty;
 }
 
+// от b до а
+string ReverseNums(int a, int b)
+{
+    if (a <= b) return ReverseNums(a + 1, b) + $"{a} ";
+    else return String.Empty;
+}
+
+// или так:
+string Nums(int a, int b)
+{   
+    if(a <= b) return $"{b} " + StringNumFromAToBRec(a, b - 1);
+    else return String.Empty;
+}
