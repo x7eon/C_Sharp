87952be1f23255e1fbebbf55ff983f7f447f346c
Alex <sungromm@gmail.com>
2022-12-23 21:13:39 +0300

    added solve

---
+++ Seminar9/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿int num = GetUserNumber("Введите число", "Ошибка ввода: ");
+
+for (int i = 1; i <= num; i++)
+{
+    Console.WriteLine(String.Join(" ", i));
+}
+
+
+string NumbersFrom1ToN(int n)
+{
+    int i = 1;
+    if (i <= n) return $"{i}" + NumbersFrom1ToN(n - 1);
+    else return String.Empty;
+}
+
+
+int GetUserNumber(string msg, string errorMsg)
+{
+    while (true)
+    {
+        Console.WriteLine(msg);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect) return userNumber;
+        Console.WriteLine(errorMsg);
+    }
+}
+
