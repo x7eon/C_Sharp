c37c56dd419ae6484d4c2fa7828c7d8271759bb1
Alex <sungromm@gmail.com>
2022-12-28 21:57:48 +0300

    added solve

---
+++ Seminar9/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+
+int num = int.Parse(Console.ReadLine()?? "");
+int answer = SumOfDigit(num);
+Console.WriteLine(answer);
+
+int SumOfDigit(int number) 
+{
+if (number / 10 == 0) return number % 10;
+else return number % 10 + SumOfDigit(number / 10);
+}
+
