a721fb69dce46a7ca6c943ed57fd29f344908fa7
Alex <sungromm@gmail.com>
2022-12-28 22:15:26 +0300

    added solve

---
+++ Seminar9/Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()?? "");
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()?? "");
+int result = APowerBRec(a, b, 0);
+Console.WriteLine(result);
+
+int APowerBRec(int a, int b, int count)
+{
+    if (count == b) return 1;
+    else return a * APowerBRec(a, b, count + 1);
+}
