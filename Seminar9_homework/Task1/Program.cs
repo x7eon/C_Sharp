27932e66741471aa27ea8f906d3d5d1b3863a338
Alex <sungromm@gmail.com>
2022-12-28 22:48:40 +0300

    added solve

---
+++ Seminar9_homework/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число m: ");
+int firstNum = int.Parse(Console.ReadLine()?? "");
+Console.WriteLine("Введите число n: ");
+int secondNum = int.Parse(Console.ReadLine()?? "");
+
+int answer = SumOfDigit(firstNum, secondNum);
+Console.WriteLine(answer);
+
+int SumOfDigit(int m, int n) 
+{
+    if (m > n) return 0;
+    else return m + SumOfDigit(m + 1, n);
+}
